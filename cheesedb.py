7960b17cc788c29082f5ecf8fef9d763c5cee9d4
Jazpy <jaz.medtre@gmail.com>
2015-10-15 21:55:16 -0500

    worked on functions that handle the database information, removed database connection unit tests

--- cheesedb.py
+++ cheesedb.py
@@ -1,19 +1,14 @@
 import sqlite3
+import table_objects
 
 def create_db(db):
 	cursor = db.cursor()
-	cursor.execute('''
-		CREATE TABLE cheeses(id INTEGER PRIMARY KEY, name TEXT,
-			country TEXT, recipes TEXT)
-		''')
-	cursor.execute('''
-		CREATE TABLE countries(id INTEGER PRIMARY KEY, name TEXT,
-			cheeses TEXT)
-		''')
-	cursor.execute('''
-		CREATE TABLE recipes(id INTEGER PRIMARY KEY, name TEXT,
-			cheese TEXT, instructions TEXT)
-		''')
+	cursor.execute('CREATE TABLE cheeses(name TEXT, softness TEXT)')
+	cursor.execute('CREATE TABLE countries(name TEXT)')
+	cursor.execute('''CREATE TABLE recipes(name TEXT, instructions TEXT,
+		time TEXT)''')
+	cursor.execute('CREATE TABLE cheese-country(chid INT, coid INT)')
+	cursor.execute('CREATE TABLE cheese-recipes(chid INT, reid INT)')
 	db.commit();
 
 
@@ -30,36 +25,86 @@ def db_exists(filename):
 
 	return header[:16] == 'SQLite format 3\x00'
 
-def add_cheese_row(db, name, country, recipes):
-	return 
+def connect_cheese_country(cheese, country):
+	cursor = db.cursor()
+
+	cursor.execute('insert into cheese-country values (? ,?)',
+		(cheese.get_id(), country.get_id()))
+
+def connect_cheese_recipe(cheese, recipe):
+	cursor = db.cursor()
+
+	cursor.execute('insert into cheese-recipes values (? ,?)',
+		(cheese.get_id(), recipe.get_id()))
+
+def add_cheese_row(db, cheese):
+	cursor = db.cursor()
+
+	#first we add to cheese table
+	cursor.execute('insert into cheeses values (?, ?)',
+		(str(cheese.get_name()), str(cheese.get_softness())))
+
+	#get the id sqlite automatically assigned
+	cheese.set_id(cursor.lastrowid))
 
 def del_cheese_row(db, cheese_to_del):
-	cursor = db.cursor
+	cursor = db.cursor()
 	
-	cursor.execute("delete from cheeses where name = " +
-		str(cheese_to_del))
+	cursor.execute("delete from cheeses where name = ?",
+		(str(cheese_to_del.get_name()),))
 
-def add_country_row(db, country, cheeses):
-	return 
+	cursor.execute("delete from cheese-country where chid = ?",
+		(cheese_to_del.get_id(),))
+
+	cursor.execute("delete from cheese-recipes where chid = ?",
+		(cheese_to_del.get_id(),))
+
+def add_country_row(db, country):
+	cursor = db.cursor()
+
+	#first we add to country table
+	cursor.execute('insert into countries values (?)',
+		(str(country.get_name())))
+
+	#get the id sqlite automatically assigned
+	country.set_id(cursor.lastrowid))
 
 def del_country_row(db, country_to_del):
-	cursor.execute("delete from countries where name = " +
-		str(country_to_del))
+	cursor = db.cursor()
 
-def add_recipe_row(db, name, cheese, instructions):
-	return 
+	cursor.execute("delete from countries where name = ",
+		(str(country_to_del.get_name()),))
+	cursor.execute("delete from cheese-country where coid = ?",
+		(country_to_del.get_id(),))
+
+def add_recipe_row(db, recipe):
+	cursor = db.cursor()
+
+	#first we add to recipe table
+	cursor.execute('insert into recipes values (?, ?, ?)',
+		(str(recipe.get_name()), str(recipe.get_softness()),
+		str(recipe.get_time())))
+
+	#get the id sqlite automatically assigned
+	recipe.set_id(cursor.lastrowid))
 
 def del_recipe_row(db, recipe_to_del):
-	cursor.execute("delete from recipes where name = " +
-		str(recipe_to_del))
+	cursor = db.cursor()
+
+	cursor.execute("delete from recipes where name = ",
+		(str(recipe_to_del.get_name()),))
+
+	cursor.execute("delete from cheese-recipes where reid = ?",
+		(recipe_to_del.get_id(),))
 
 if db_exists('db/database'):
 	db = sqlite3.connect('db/database')
-	cursor = db.cursor
+	cursor = db.cursor()
 else:
 	db = sqlite3.connect("db/database")
 	create_db(db)
-	cursor = db.cursor
+	cursor = db.cursor()
+
 
 	
 db.close;
--- test_cheesedb.py
+++ test_cheesedb.py
@@ -6,74 +6,7 @@ import sqlite3
 class TestCDB(unittest.TestCase):
 
 	def setUp(self):
-		if cheesedb.db_exists('db/database'):
-			self.db = sqlite3.connect("db/database")
-		else:
-			self.db = sqlite3.connect("db/database")
-			cheesedb.create_db(self.db)
-
-	def tearDown(self):
-		self.db.close()
-
-	def test_db_exists(self):
-		self.assertEqual(cheesedb.db_exists('db/database'), True)
-
-	def test_cheese_table(self):
-		cursor = self.db.cursor()
-
-		cheesedb.add_cheese_row(self.db, "testcheese", "no", "no")
-
-		cursor.execute(
-			"select id from cheeses where name = 'testcheese'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is not None, True)
-
-		cheesedb.del_cheese_row(self.db, "testcheese")
-
-		cursor.execute(
-			"select id from cheeses where name = 'testcheese'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is None, True)
-
-	def test_country_table(self):
-		cursor = self.db.cursor()
-
-		cheesedb.add_country_row(self.db, "testcountry", "a, b")
-
-		cursor.execute(
-			"select id from countries where name = 'testcountry'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is not None, True)
-
-		cheesedb.del_cheese_row(self.db, "testcountry")
-
-		cursor.execute(
-			"select id from countries where name = 'testcountry'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is None, True)
-
-	def test_recipe_table(self):
-		cursor = self.db.cursor()
-
-		cheesedb.add_cheese_row(self.db, "testrecipe", "no", "no")
-
-		cursor.execute(
-			"select id from recipes where name = 'testrecipe'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is not None, True)
-
-		cheesedb.del_cheese_row(self.db, "testrecipe")
-
-		cursor.execute(
-			"select id from recipes where name = 'testrecipe'")
-		exist = cursor.fetchone()
-
-		self.assertEqual(exist is None, True)
+		pass
 
 	def test_table_object_cheese(self):
 		x = table_objects.cheese(1, "a", "b")
@@ -88,6 +21,34 @@ class TestCDB(unittest.TestCase):
 		self.assertEqual(x.get_name() == "b", True)
 		x.set_softness("a")
 		self.assertEqual(x.get_softness() == "a", True)
+
+	def test_table_object_country(self):
+		x = table_objects.country(1, "a")
+		
+		self.assertEqual(x.get_id() == 1, True)
+		self.assertEqual(x.get_name() == "a", True)
+
+		x.set_id(2)
+		self.assertEqual(x.get_id() == 2, True)
+		x.set_name("b")
+		self.assertEqual(x.get_name() == "b", True)
+	
+	def test_table_object_recipe(self):
+		x = table_objects.recipe(1, "a", "b", 1)
+		
+		self.assertEqual(x.get_id() == 1, True)
+		self.assertEqual(x.get_name() == "a", True)
+		self.assertEqual(x.get_instructions() == "b", True)
+		self.assertEqual(x.get_time() == 1, True)
+
+		x.set_id(2)
+		self.assertEqual(x.get_id() == 2, True)
+		x.set_name("b")
+		self.assertEqual(x.get_name() == "b", True)
+		x.set_instructions("a")
+		self.assertEqual(x.get_instructions() == "a", True)
+		x.set_time(2)
+		self.assertEqual(x.get_time() == 2, True)
 
 if __name__ == '__main__':
 	unittest.main()
